7d40124786a2a4660dfe75bfa0d1b8c34ff7f2f4
阮迎 <654803982@qq.com>
2020-07-12 17:56:42 +0800

    更新两个服务

---
+++ myspringcloud/cloud-consumer-order80/src/main/java/com/atry/springcloud/OrderMain80.java
@@ -0,0 +1,11 @@
+package com.atry.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class, args);
+
+    }
+}
