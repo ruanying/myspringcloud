f86b32e1f40bf576977a43317b683acd7da4fbe3
阮迎 <654803982@qq.com>
2020-07-14 00:34:31 +0800

    完成服务注册

--- myspringcloud/cloud-consumer-order80/src/main/java/com/atry/springcloud/OrderMain80.java
+++ myspringcloud/cloud-consumer-order80/src/main/java/com/atry/springcloud/OrderMain80.java
@@ -3,8 +3,11 @@ package com.atry.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
