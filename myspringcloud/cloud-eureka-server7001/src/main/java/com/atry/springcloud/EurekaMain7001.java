bf68501651325083da5412f2e7b560657d83a019
阮迎 <654803982@qq.com>
2020-07-21 21:10:53 +0800

    完成集群服务注册

--- myspringcloud/cloud-eureka-server7001/src/main/java/com/atry/springcloud/EurekaMain7001.java
+++ myspringcloud/cloud-eureka-server7001/src/main/java/com/atry/springcloud/EurekaMain7001.java
@@ -1,4 +1,4 @@
-package com.atry.sringcloud;
+package com.atry.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
